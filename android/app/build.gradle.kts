708245c5310ca29c4e7fba1d77c018c2aa6289b0
Volodia Kraplich <v1mkss.m+git@gmail.com>
2025-04-05 22:45:14 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "dev.crystalnetwork.mobile.simpledictionary"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
